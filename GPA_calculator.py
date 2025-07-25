f01eb114c22a4769516531f895584d5d9c4a1d28
Khoi <lhp.khoi730@gmail.com>
2025-07-26 01:52:39 +0700

    Update footer

--- GPA_calculator.py
+++ GPA_calculator.py
@@ -140,8 +140,51 @@ def web():
                 """ )
         except KeyError:
             pass
-            
-    st.markdown("Copyright © khoi.nguyenminhcs22@hcmut.edu.vn")
+        
+    ft = """
+    <style>
+    a:link , a:visited{
+    color: #BFBFBF;  /* theme's text color hex code at 75 percent brightness*/
+    background-color: transparent;
+    text-decoration: none;
+    }
+
+    a:hover,  a:active {
+    color: #0283C3; /* theme's primary color*/
+    background-color: transparent;
+    text-decoration: underline;
+    }
+
+    #page-container {
+    position: relative;
+    min-height: 10vh;
+    }
+
+    footer{
+        visibility:hidden;
+    }
+
+    .footer {
+    position: relative;
+    left: 0;
+    top:230px;
+    bottom: 0;
+    width: 100%;
+    background-color: transparent;
+    color: #808080; /* theme's text color hex code at 50 percent brightness*/
+    text-align: left; /* you can replace 'left' with 'center' or 'right' if you want*/
+    }
+    </style>
+
+    <div id="page-container">
+
+    <div class="footer">
+    <p style='font-size: 0.875em;'>Made by <img src="https://em-content.zobj.net/source/skype/289/red-heart_2764-fe0f.png" alt="heart" height= "10"/><a style='display: inline; text-align: left;' href="https://github.com/ngmikhoi" target="_blank"> ngmikhoi</a></p>
+    </div>
+
+    </div>
+    """
+    st.write(ft, unsafe_allow_html=True)
 
     
 if __name__ == "__main__":    
@@ -166,6 +209,3 @@ if __name__ == "__main__":
     import pandas as pd
     print("All required packages installed.")  
     web()
-    
-    
-    
